e6ec6b99695ab1acee6801695bb9e9a38512d677
Semen Protsenko <semen.protsenko@globallogic.com>
2015-02-20 19:32:40 +0200

    Fix stack address for XIP boot (from NOR).
    Signed-off-by: Semen Protsenko <semen.protsenko@globallogic.com>

--- include/config.h
+++ include/config.h
@@ -1,17 +1,17 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+#define CONFIG_STACK_ADDR		0x40380000 /* Non-secure SRAM end */
+
 #if (CONFIG_BOOT == 1)		/* SD card */
 
 #define CONFIG_SRAM_BASE		0x40300350
 #define CONFIG_SDRAM_BASE		0x84000000
-#define CONFIG_STACK_ADDR		0x40380000 /* Non-secure SRAM end */
 
 #elif (CONFIG_BOOT == 2)	/* NOR (XIP) */
 
 #define CONFIG_SRAM_BASE		0x08000000
 #define CONFIG_SDRAM_BASE		0x08100000
-#define CONFIG_STACK_ADDR		0x08200000
 
 #else
 
