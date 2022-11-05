cfabf2b97e29385529f3e2742e68609884eed1f2
Sam Protsenko <semen.protsenko@linaro.org>
2022-11-05 12:19:04 +0100

    build: gen-lds: Use -P gcc flag to suppress line directives
    Instead of removing all '#...' lines from the preprocessor output with
    grep, one can use -P gcc option for the same purpose.
    
    Signed-off-by: Sam Protsenko <semen.protsenko@linaro.org>

--- build/gen-lds.sh
+++ build/gen-lds.sh
@@ -14,4 +14,4 @@ cflags=$*
 echo "/* This file was generated automatically."	>$out
 echo " * Please do not modify it manually!"		>>$out
 echo " */"						>>$out
-gcc -E -x c -Iinclude $cflags $in | grep -v '^#'	>>$out
+gcc -E -P -x c -Iinclude $cflags $in			>>$out
